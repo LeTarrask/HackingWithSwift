2514359efdf5dbdee1cdd9d29fd91c966293ab31
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2021-06-18 13:15:16 +0100

    Finished MacOS project 11

---
+++ MacOS_Projects/SpriteKitCleanTemplate/SpriteKitCleanTemplate/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  SpriteKitCleanTemplate
+//
+//  Created by tarrask on 18/06/2021.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+   override func didMove(to view: SKView) {
+   }
+}
