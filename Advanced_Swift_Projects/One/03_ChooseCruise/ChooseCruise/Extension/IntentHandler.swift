d9fc76d6037407b6a4cc13dee374151f98990964
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2021-06-23 12:35:03 +0100

    Finished Advanced #3

---
+++ Advanced_Swift_Projects/One/03_ChooseCruise/ChooseCruise/Extension/IntentHandler.swift
@@ -0,0 +1,111 @@
+//
+//  IntentHandler.swift
+//  Extension
+//
+//  Created by tarrask on 23/06/2021.
+//
+
+import Intents
+import UIKit
+
+class IntentHandler: INExtension, INRidesharingDomainHandling {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+    func handle(intent: INListRideOptionsIntent, completion: @escaping (INListRideOptionsIntentResponse) -> Void) {
+        
+        let result = INListRideOptionsIntentResponse(code: .success, userActivity: nil)
+        let mini = INRideOption(name: "Mini Cooper", estimatedPickupDate: Date(timeIntervalSinceNow: 1000))
+        let accord = INRideOption(name: "Honda Accord", estimatedPickupDate: Date(timeIntervalSinceNow: 800))
+        let ferrari = INRideOption(name: "Ferrari F430", estimatedPickupDate: Date(timeIntervalSinceNow: 300))
+        ferrari.disclaimerMessage = "This is bad for the environment"
+        
+        result.expirationDate = Date(timeIntervalSinceNow: 3600)
+        result.rideOptions = [mini, accord, ferrari]
+        completion(result)
+    }
+    
+    func handle(intent: INRequestRideIntent, completion: @escaping (INRequestRideIntentResponse) -> Void) {
+        let result = INRequestRideIntentResponse(code: .success, userActivity: nil)
+        let status = INRideStatus()
+        // this is our internal value that identifies the ride uniquely in our back-end system
+        status.rideIdentifier = "abc123"
+        // give it the pick up and drop off location we already agreed with the user
+        status.pickupLocation = intent.pickupLocation
+        status.dropOffLocation = intent.dropOffLocation
+        // mark it as confirmed – we will deliver a ride
+        status.phase = INRidePhase.confirmed
+        // say we'll be there in 15 minutes
+        status.estimatedPickupDate = Date(timeIntervalSinceNow: 900)
+        // create a new vehicle and configure it fully
+        let vehicle = INRideVehicle()
+        // workaround: load the car image into UIImage, convert that into PNG data, then create an INImage out of that
+        let image = UIImage(named: "car")!
+        let data = image.pngData()!
+        vehicle.mapAnnotationImage = INImage(imageData: data)
+        // set the vehicle's current location to wherever the user wants to go – this ought to at least place it a little way away for testing purposes
+        vehicle.location = intent.dropOffLocation!.location
+        // now that we have configured the vehicle fully, assign it all at once to status.vehicle
+        status.vehicle = vehicle
+        
+        // attach our finished INRideStatus object to the result
+        result.rideStatus = status
+        // and send it back
+        completion(result)
+    }
+    
+    func resolvePickupLocation(for intent: INRequestRideIntent, with completion: @escaping (INPlacemarkResolutionResult) -> Void) {
+        let result: INPlacemarkResolutionResult
+        if let requestedLocation = intent.pickupLocation {
+            // we have a valid pick up location – return success!
+            result = INPlacemarkResolutionResult.success(with: requestedLocation)
+        } else {
+            // no pick up location yet; mark this as outstanding
+            result = INPlacemarkResolutionResult.needsValue()
+        }
+        completion(result)
+    }
+    
+    func resolveDropOffLocation(for intent: INRequestRideIntent, with completion: @escaping (INPlacemarkResolutionResult) -> Void) {
+        let result: INPlacemarkResolutionResult
+        if let requestedLocation = intent.dropOffLocation {
+            result = INPlacemarkResolutionResult.success(with: requestedLocation)
+        } else {
+            result = INPlacemarkResolutionResult.needsValue()
+        }
+        completion(result)
+    }
+    
+    // Methods that can't be faked about the ride, and we won't use in the tutorial, just to make XCode shut up.
+    func handle(intent: INGetRideStatusIntent, completion: @escaping (INGetRideStatusIntentResponse) -> Void) {
+        
+        // This code just replies "everything's fine" when user asks about their ride.
+        let result = INGetRideStatusIntentResponse(code: .success, userActivity: nil)
+        completion(result)
+    }
+    
+    func startSendingUpdates(for intent: INGetRideStatusIntent, to observer: INGetRideStatusIntentResponseObserver) {
+    }
+    
+    func stopSendingUpdates(for intent: INGetRideStatusIntent) {
+    }
+    
+    func handle(cancelRide intent: INCancelRideIntent, completion: @escaping (INCancelRideIntentResponse) -> Void) {
+        
+        // This code just replies "everything's fine" when user asks about their ride.
+        let result = INCancelRideIntentResponse(code: .success, userActivity: nil)
+        completion(result)
+    }
+    
+    func handle(sendRideFeedback sendRideFeedbackintent: INSendRideFeedbackIntent, completion: @escaping (INSendRideFeedbackIntentResponse) -> Void) {
+        
+        // This code just replies "everything's fine" when user asks about their ride.
+        let result = INSendRideFeedbackIntentResponse(code: .success, userActivity: nil)
+        completion(result)
+    }
+}
---
+++ Advanced_Swift_Projects/One/03_ChooseCruise/ChooseCruise/Shared/ChooseCruiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseCruiseApp.swift
+//  Shared
+//
+//  Created by tarrask on 23/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ChooseCruiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
